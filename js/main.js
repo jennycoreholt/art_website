cc0f39f9c19fb925f989b6bfc2b31d35a1d38eef
Jenny Core-Holt <jenny@jennycore.com>
2018-04-10 21:48:36 +0200

    carousel image lazy load updates

--- js/main.js
+++ js/main.js
@@ -21,4 +21,15 @@ $(document).ready(function() {
 		$(this).toggleClass('open');
 	});
 
+  //carousel//
+  $(function() {
+    return $(".carousel.lazy").on("slide.bs.carousel", function(ev) {
+      var lazy;
+      lazy = $(ev.relatedTarget).find("img[data-src]");
+      lazy.attr("src", lazy.data('src'));
+      lazy.removeAttr("data-src");
+    });
+  });
+
+
 });
